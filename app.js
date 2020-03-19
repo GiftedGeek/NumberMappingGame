f1fbbf3c218dee8bbb6b841eb199e31a7840dca3
GiftedGeek <40801382+GiftedGeek@users.noreply.github.com>
2020-03-20 01:32:44 +0530

    Updated Logics

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var container = document.getElementById("gameDivision");
 
 var gameButton = document.getElementById('start')
-document.getElementById('start').innerHTML = "Start Game"
+document.getElementById('start').innerHTML = "Start"
 gameButton.setAttribute('onclick', 'startGame ();')
 
-
+//Game Beginning
 function startGame() {
     for (let i = 0; i < 16; i++) {
         var digitDivs = document.createElement('div')
@@ -15,8 +15,11 @@ function startGame() {
     }
     gameButton.setAttribute('disabled', 'true')
     restartButton();
+    answerGenerator();
+    console.log(ansArr)
 };
 
+//Number Generation
 function generator(length) {
     var result = '';
     var characters = '0123456789';
@@ -27,12 +30,26 @@ function generator(length) {
     return result;
 }
 
-let genString = generator(4);
-var ansArr = genString.split('')
+//Answer Array Generation
+var ansArr = [0, 1, 2, 3]
+function answerGenerator() {
+    let genString = generator(4);
+    ansArr = genString.split('')
+}
 
+
+//Logicals
 function divClick(divId) {
     (!hasValue(divId)) ?
-        document.getElementById(divId).innerHTML = generator(1) : ''
+        document.getElementById(divId).innerHTML = randomNumber : ''
+    var randomNumber = generator(1);
+    var ind = divId.split('d')[1] % 4;
+    if (randomNumber == ansArr[ind]) {
+        document.getElementById(divId).setAttribute('style', 'background-color: #69FF39');
+    } else {
+        document.getElementById(divId).setAttribute('style', 'background-color: #FF3939;');
+    }
+    document.getElementById(divId).innerHTML = randomNumber;
 }
 
 
@@ -46,7 +63,8 @@ function restartButton() {
     var getContainer = document.getElementById('container')
     var restartButton = document.createElement('button')
     restartButton.id = 'restart'
-    restartButton.innerText = "Restart Game"
+    restartButton.className = 'myButn'
+    restartButton.innerText = "Restart"
     restartButton.setAttribute('onclick', 'restartGame()')
     getContainer.appendChild(restartButton)
 }
@@ -55,5 +73,9 @@ function restartButton() {
 function restartGame() {
     for (let i = 0; i < 16; i++) {
         document.getElementById(`id${i}`).innerHTML = ''
+        document.getElementById(`id${i}`).setAttribute('style', 'background-color: #5D5D5D;')
     }
+    ansArr = []
+    answerGenerator()
+    console.log(ansArr)
 }
